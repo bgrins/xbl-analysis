b16f29e3958a84a396ca7db0a9c55b5191148962
Brian Grinstead <briangrinstead@gmail.com>
2017-09-22 10:35:15 -0700

    add info about loc per binding

--- scripts/build-xbl-graph.js
+++ scripts/build-xbl-graph.js
@@ -8,20 +8,39 @@ var data = {};
 function countForRev(rev) {
   console.log(`Looking at ${rev}`);
   return getParsedFiles(rev).then(files => {
+    let bindingsLOC = new Map();
     let numBindings = files.map(file => {
-      return file.doc.find('binding').length;
+      let lameBindingParse = file.body.split('<binding ').slice(1);
+      let docBindings = file.doc.find('binding');
+      if (lameBindingParse.length != docBindings.length) {
+        throw `Splitting on '<binding ' did not work (${lameBindingParse.length} vs ${docBindings.length})`;
+      }
+      docBindings.forEach((binding, i) => {
+        bindingsLOC.set(binding.attrs.id, lameBindingParse[i].split(/\n/).length);
+      })
+      return docBindings.length;
     }).reduce((a, b) => { return a + b; });
-    let loc = files.map(file => {
-      return file.body.split(/\n/).length;
-    }).reduce((a, b) => { return a + b; });
+    let loc = [...bindingsLOC.values()].reduce((a, b) => { return a + b; });
     let label = rev.match(/@{(.*)}/)[1];
-
+    console.log(loc);
     data[rev] = {
       numBindings,
       loc,
-      label
+      bindingsLOC: mapToObj(bindingsLOC),
+      label,
     };
   });
+}
+
+function mapToObj(map) {
+  let obj = Object.create(null);
+  map = [...map.entries()].sort(([key1, value1], [key2, value2]) => {
+    return value2 - value1;
+  });
+  for (let [k,v] of map) {
+      obj[k] = v;
+  }
+  return obj;
 }
 
 let revs = [
@@ -40,10 +59,15 @@ Promise.all(
   })
 ).then(() => {
   for (var rev of revs) {
-    console.log(rev, data[rev]);
+  }
+
+  let inOrder = {};
+  for (var rev of revs) {
+    console.log(rev, data[rev].label, data[rev].numBindings, data[rev].loc);
+    inOrder[rev] = data[rev];
   }
 
   fs.writeFileSync('graph/xbl-counts.js', prettier.format(`
-    var DATA = ${JSON.stringify(data)};
+    var DATA = ${JSON.stringify(inOrder)};
   `));
 });
