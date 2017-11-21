54a4b5db5285dd3385aaee1eb82ca976c1ae1f50
Brian Grinstead <briangrinstead@gmail.com>
2017-11-21 11:46:47 -0800

    blow away cache and npm run all

--- scripts/populate-cache.js
+++ scripts/populate-cache.js
@@ -113,12 +113,12 @@ async function populateUsageData(rev) {
 }
 
 async function populateCaches(rev) {
-  console.log(`Caching bindings for ${revsEveryDay.revs}`);
+  console.log(`Caching bindings for ${revsEveryDay.length} revs`);
   await revsEveryDay.reduce(function (chain, item) {
     return chain.then(populateBindingCache.bind(null, item));
   }, populateBindingCache(revsEveryDay[0]));
 
-  console.log(`Caching usage data for ${revsEveryDay.revs}`);
+  console.log(`Caching usage data for ${revsEveryDay.length} revs`);
   await revsEveryDay.reduce(function (chain, item) {
     return chain.then(populateUsageData.bind(null, item));
   }, populateUsageData(revsEveryDay[0]));
