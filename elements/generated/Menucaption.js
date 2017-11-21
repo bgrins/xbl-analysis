b78b2f79d9f937b25b2cc633a02c5018c530e354
Brian Grinstead <briangrinstead@gmail.com>
2017-11-21 08:56:58 -0800

    run all

--- elements/generated/Menucaption.js
+++ elements/generated/Menucaption.js
@@ -2,7 +2,11 @@ class FirefoxMenucaption extends FirefoxMenuBase {
   connectedCallback() {
     super.connectedCallback();
     this.innerHTML = `
-      <xul:label class="menu-text" inherits="value=label,crop" crop="right"></xul:label>
+      <xul:hbox class="menu-iconic-left" align="center" pack="center" inherits="selected,disabled,checked">
+        <xul:image class="menu-iconic-icon" inherits="src=image,validate,src"></xul:image>
+      </xul:hbox>
+      <xul:label class="menu-iconic-text" flex="1" inherits="value=label,crop,highlightable" crop="right"></xul:label>
+      <xul:label class="menu-iconic-highlightable-text" inherits="text=label,crop,highlightable" crop="right"></xul:label>
     `;
   }
 }
