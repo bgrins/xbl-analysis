57ed65a35d8d4c4e63fbea141f3324c661489e69
Brian Grinstead <briangrinstead@gmail.com>
2017-11-21 11:42:56 -0800

    pull down usagedata from taskcluster

--- scripts/populate-cache.js
+++ scripts/populate-cache.js
@@ -1,14 +1,23 @@
 
 var fs = require('fs');
-var { revsEveryDay, getAllFilesForRev } = require('./xbl-files');
+var { revsEveryDay, getAllFilesForRev, getPrettyRev} = require('./xbl-files');
+var request = require('request-promise-native');
+const activeData = 'https://activedata.allizom.org/query';
+var moment = require("moment");
 
+if (!fs.existsSync('cache')) {
+  fs.mkdirSync('cache');
+}
+if (!fs.existsSync('cache/instrumentation')) {
+  fs.mkdirSync('cache/instrumentation');
+}
 
-function populateCache(rev) {
+function populateBindingCache(rev) {
   if (!rev) {
     throw "Need a rev";
   }
 
-  console.log(`Populating ${rev}`);
+  console.log(`populateBindingCache for ${rev}`);
 
   let files = getAllFilesForRev(rev);
   // Allow for revisions like 'master@{2017-09-19}'
@@ -16,9 +25,6 @@ function populateCache(rev) {
     return file.replace('/master/', `/${rev}/`);
   });
 
-  if (!fs.existsSync('cache')) {
-    fs.mkdirSync('cache');
-  }
   var dir = `cache/${rev}`;
   if (!fs.existsSync(dir)) {
     console.log(`Creating directory: ${dir}`);
@@ -43,13 +49,78 @@ function populateCache(rev) {
   }));
 }
 
-function processRev(rev) {
-  return populateCache(rev);
+async function populateUsageData(rev) {
+  if (!rev) {
+    throw "Need a rev";
+  }
+  rev = getPrettyRev(rev);
+
+  // Don't have data until now
+  if (moment(rev) < moment("2017-11-17") || moment(rev) > moment().subtract(1, 'days')) {
+    return;
+  }
+
+  console.log(`Fetching usage data for ${rev}`);
+
+  var cachedFilePath = `cache/instrumentation/${rev}/xulsummary.txt`;
+  if (fs.existsSync(cachedFilePath)) {
+    console.log(`File already exists: ${cachedFilePath}`);
+    return;
+  }
+
+  const tomorrowRev = moment(rev).add(1, 'days').format('YYYY-MM-DD');
+  const query = {
+    "sort": { "repo.push.date": "desc" },
+    "limit": 1,
+    "from": "task.task.artifacts",
+    "where": {
+      "and": [
+        { "eq": { "name": "public/test_info/xulsummary.txt" } },
+        { "gte": { "repo.push.date": { "date": rev } } },
+        { "lt": { "repo.push.date": { "date": tomorrowRev } } },
+      ]
+    },
+    "select": ["task.artifacts.url", "repo"],
+    format: 'list',
+  }
+
+  const queryResult = await request({
+    url: activeData,
+    method: "POST",
+    json: query,
+  });
+  let firstMcPush = queryResult.data.filter(d => d.repo.branch.name === "mozilla-central")[0];
+  if (!firstMcPush) {
+    console.log(`Skipping result for ${rev} (${queryResult.data.length} pushes, 0 on m-c)`, JSON.stringify(queryResult));
+    return;
+  }
+
+  let { url } = firstMcPush.task.artifacts;
+  console.log(`Found artifact at ${url}`);
+  if (!url) {
+    throw `Invalid data ${JSON.stringify(queryResult)}`;
+  }
+
+  const artifactData = await request({
+    url: url,
+    gzip: true,
+  });
+
+  if (!fs.existsSync(`cache/instrumentation/${rev}`)) {
+    fs.mkdirSync(`cache/instrumentation/${rev}`);
+  }
+  fs.writeFileSync(cachedFilePath, `Fetched from ${url}\n\n${artifactData}`);
 }
 
-const revsToProcess = revsEveryDay.slice(0);
+async function populateCaches(rev) {
+  console.log(`Caching bindings for ${revsEveryDay.revs}`);
+  await revsEveryDay.reduce(function (chain, item) {
+    return chain.then(populateBindingCache.bind(null, item));
+  }, populateBindingCache(revsEveryDay[0]));
 
-// Cache files in sequence
-return revsToProcess.reduce(function (chain, item) {
-  return chain.then(processRev.bind(null, item));
-}, processRev(revsToProcess.shift()));
+  console.log(`Caching usage data for ${revsEveryDay.revs}`);
+  await revsEveryDay.reduce(function (chain, item) {
+    return chain.then(populateUsageData.bind(null, item));
+  }, populateUsageData(revsEveryDay[0]));
+}
+populateCaches();
