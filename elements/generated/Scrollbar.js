7859fb31a8c8d3c17858c4ee808173d188e2672d
Brian Grinstead <briangrinstead@gmail.com>
2018-03-05 10:13:18 -0800

    run all

--- elements/generated/Scrollbar.js
+++ elements/generated/Scrollbar.js
@@ -2,13 +2,13 @@ class FirefoxScrollbar extends XULElement {
   connectedCallback() {
 
     this.innerHTML = `
-      <xul:scrollbarbutton sbattr="scrollbar-up-top" type="decrement" inherits="curpos,maxpos,disabled,sborient=orient"></xul:scrollbarbutton>
-      <xul:scrollbarbutton sbattr="scrollbar-down-top" type="increment" inherits="curpos,maxpos,disabled,sborient=orient"></xul:scrollbarbutton>
-      <xul:slider flex="1" inherits="disabled,curpos,maxpos,pageincrement,increment,orient,sborient=orient">
-        <xul:thumb sbattr="scrollbar-thumb" inherits="orient,sborient=orient,collapsed=disabled" align="center" pack="center"></xul:thumb>
+      <xul:scrollbarbutton sbattr="scrollbar-up-top" type="decrement" inherits="curpos,maxpos,disabled"></xul:scrollbarbutton>
+      <xul:scrollbarbutton sbattr="scrollbar-down-top" type="increment" inherits="curpos,maxpos,disabled"></xul:scrollbarbutton>
+      <xul:slider flex="1" inherits="disabled,curpos,maxpos,pageincrement,increment,orient">
+        <xul:thumb sbattr="scrollbar-thumb" inherits="orient,collapsed=disabled" align="center" pack="center"></xul:thumb>
       </xul:slider>
-      <xul:scrollbarbutton sbattr="scrollbar-up-bottom" type="decrement" inherits="curpos,maxpos,disabled,sborient=orient"></xul:scrollbarbutton>
-      <xul:scrollbarbutton sbattr="scrollbar-down-bottom" type="increment" inherits="curpos,maxpos,disabled,sborient=orient"></xul:scrollbarbutton>
+      <xul:scrollbarbutton sbattr="scrollbar-up-bottom" type="decrement" inherits="curpos,maxpos,disabled"></xul:scrollbarbutton>
+      <xul:scrollbarbutton sbattr="scrollbar-down-bottom" type="increment" inherits="curpos,maxpos,disabled"></xul:scrollbarbutton>
     `;
 
     this._setupEventListeners();
